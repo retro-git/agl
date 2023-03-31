bccdec45d3605a4be8ef6fb4a4275255f6a0576d
retro <retro@pc-71-138.customer.ask4.lan>
2023-03-31 20:39:34 +0100

    Cur

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use std::collections::HashMap;
+
 #[macro_use] extern crate lalrpop_util;
 
 lalrpop_mod!(pub parser); // synthesized by LALRPOP
 
 fn main() {
     println!("Hello, world!");
-    parser::ProgramParser::new().parse("12").unwrap();
+    parser::ProgramParser::new().parse(&HashMap::new(), "12").unwrap();
 }
 
 #[test]
 fn test() {
-    let parsed = parser::ProgramParser::new().parse("x = 14 + 7 * 2;").unwrap();
+    let parsed = parser::ProgramParser::new().parse(&HashMap::new(), "x = 14 + 7 * 2;").unwrap();
     // debug print
     println!("{:?}", parsed);
     assert!(false);
