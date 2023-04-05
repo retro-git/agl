b08b8d7a96fc8ee383fd3891740e45ebdfa53c29
retro <retro@pc-71-138.customer.ask4.lan>
2023-04-05 01:23:28 +0100

    remove site

--- site/src/routes/Compile.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import init, { compile } from 'agl';
-    //prop to hold string to compile
-    export let code = "";
-</script>
-
-<p>{compile(code)}</p>
--- site/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import wasmPack from 'vite-plugin-wasm-pack';
-
-export default defineConfig({
-	plugins: [sveltekit(), wasmPack('../agl')]
-});
